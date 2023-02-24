2edd273f4c0704ef8f083249e7ad0811e692ead2
kubra18 <ktanribuyurdu@gmail.com>
2023-02-24 21:12:21 +0100

    24.02

--- batch103Cucumber/src/test/java/runners/Runner.java
+++ batch103Cucumber/src/test/java/runners/Runner.java
@@ -1,2 +1,18 @@
-package runners;public class Runners {
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "./src/test/resources/features",//features folder path
+        glue = "stepdefinitions",   //stepdefinitions path
+        tags = "@iphone or @tesla"
+
+)
+public class Runner {
 }
+//Bu sinif Test caseleri RUN etmek icin kullanilir
+//Ve konfigurasyonlar icin kullanilir
+//Runner class, features file lar ile step defitions i birbirile baglar
--- batch103Cucumber/src/test/java/stepdefinitions/GoogleStepDefinitions.java
+++ batch103Cucumber/src/test/java/stepdefinitions/GoogleStepDefinitions.java
@@ -2,23 +2,44 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.GooglePage;
 import utilities.ConfigReader;
 import utilities.Driver;
-public class GoogleStepDef {
-    @Given("user google gider")
-    public void user_google_gider() {
+public class GoogleStepDefinitions {
+    GooglePage googlePage = new GooglePage();
+    @Given("kullanici google gider")
+    public void kullanici_google_gider() {
         Driver.getDriver().get(ConfigReader.getProperty("google_url"));
-
+//        CEREZ CIKAN ARKADARLAR ICIN, CEREZI OKEYLEYELIM
         try{
-
+            googlePage.popUpAccept.click();
         }catch (Exception e){
-
         }
     }
-    @When("user iphone icin arama yapar")
-    public void user_iphone_icin_arama_yapar() {
+    @When("kullanici iphone için arama yapar")
+    public void kullanici_iphone_için_arama_yapar() {
+        googlePage.searchBox.sendKeys("iPhone"+ Keys.ENTER);
     }
-    @Then("user sonuclarda iphone oldugunu dogrular")
-    public void user_sonuclarda_iphone_oldugunu_dogrular() {
+    @Then("sonuclarda iphone oldugunu dogrular")
+    public void sonuclarda_iphone_oldugunu_dogrular() {
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains("iPhone"));
     }
+    @Then("close the application")
+    public void close_the_application() {
+        Driver.closeDriver();
+    }
+    @When("kullanici tesla için arama yapar")
+    public void kullanici_tesla_için_arama_yapar() {
+        googlePage.searchBox.sendKeys("tesla"+ Keys.ENTER);
+
+    }
+    @Then("sonuclarda tesla oldugunu dogrular")
+    public void sonuclarda_tesla_oldugunu_dogrular() {
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains("tesla"));
+
+
+    }
+
 }
